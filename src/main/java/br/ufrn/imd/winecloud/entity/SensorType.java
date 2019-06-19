d5dd26bee8e10b42bf95b21a99a3e7650e8ef79d
shirleytelemaco98@gmail.com <shirleytelemaco98@gmail.com>
2019-06-18 23:24:16 -0300

    Adding the barrel class

--- src/main/java/br/ufrn/imd/winecloud/entity/SensorType.java
+++ src/main/java/br/ufrn/imd/winecloud/entity/SensorType.java
@@ -1,5 +1,12 @@
 package br.ufrn.imd.winecloud.entity;
 
+/**
+ * The SensorType enumerates the sensor types. A sensor can be typed with
+ * Humidity or Temperature
+ *
+ * @author  Shirley Ohara (shirleyohara@ufrn.edu.br)
+ * @version 18 de jun de 2019 | 23:13:03
+ */
 public enum SensorType {
 	Humidity, Temperature
 }
