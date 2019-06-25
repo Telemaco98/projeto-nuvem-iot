70aed7fd9214b4f8dbbff3aa7c93489ec55e57cc
shirleytelemaco98@gmail.com <shirleytelemaco98@gmail.com>
2019-06-25 03:45:31 -0300

    Implementing the request POST method of register barrel

--- src/main/java/br/ufrn/imd/winecloud/business/BarrelServices.java
+++ src/main/java/br/ufrn/imd/winecloud/business/BarrelServices.java
@@ -1,5 +1,14 @@
 package br.ufrn.imd.winecloud.business;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.apache.commons.io.IOUtils;
+
 import com.google.appengine.api.users.User;
 
 import br.ufrn.imd.winecloud.entity.Barrel;
@@ -23,32 +32,48 @@ public class BarrelServices {
 		this.user 	= user;
 	}
 	
-	public void registerBarrel() {
+	public void registerBarrel() throws IOException {
 		System.out.println(barrel.toString());
-		
-		
-//		Método: POST
-//		Link:   http://169.254.169.254#53:1026/v1/contextEntities
-//		Header
-//			KEY: Content-Type
-//			VALUE: applcation/json
-//		Body
-//		{ 
-//			"type": ("user.getUserId() + "_" + "barril") ", 
-//			"isPattern": "false", 
-//			"id": "barrel.getID()",
-//			"attributes": [ 
-//				{ 
-//					"name": "Medicao", 
-//					"type": "float", 
-//					"value": "25.0" }, 
-//				{ 
-//					"name": "Localizacao", 
-//					"type": "coords", 
-//					"value": "-5.8323934,-35.2076385"
-//				} 
-//			]
-//		}
 
+		String url_str 	= "http://" + ConnectionUtils.IP + ":" + ConnectionUtils.PORT_ORION +  
+				"/v1/contextEntities";
+		String json 	= 
+			"{ " +
+					"\"type\": \"" + user.getUserId() + "_" + "barrel\", " +
+					"\"isPattern\": \"false\", " +
+					"\"id\": \"" + barrel.getId() + "\", " +
+					"\"attributes\": [" + 
+						"{ " +
+							"\"name\": \"dob\"    ," + 
+							"\"type\": \"String\" ," + 
+							"\"value\": \""+ barrel.getDob() + "\" }," + 
+						"{ " +
+							"\"name\": \"grapeType\"," + 
+							"\"type\": \"String\"," + 
+							"\"value\": \"" + barrel.getGrapeType() + "\" }" + 
+					"]" +
+				"}";
+		
+		System.out.println(url_str);
+		System.out.println(json);
+		
+		URL	   url = new URL(url_str);
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestProperty("Content-Type", "application/json; charset=UFT-8");
+		con.setDoInput(true);
+		con.setDoOutput(true);
+		con.setRequestMethod("POST");
+		
+		OutputStream os = con.getOutputStream();
+		os.write(json.getBytes("UTF-8"));
+		os.close();
+		
+		InputStream is = new BufferedInputStream(con.getInputStream());
+		String result = IOUtils.toString(is, "UTF-8");
+		is.close();
+		
+		System.out.println(result);
+		
+		con.disconnect();
 	}
 }
---
+++ src/main/java/br/ufrn/imd/winecloud/business/ConnectionUtils.java
@@ -0,0 +1,6 @@
+package br.ufrn.imd.winecloud.business;
+
+public class ConnectionUtils {
+	public static String IP   = "169.254.169.254#53";
+	public static String PORT_ORION = "1026";
+}
